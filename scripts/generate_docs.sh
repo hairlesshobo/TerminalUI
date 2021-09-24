5c6bbf485c94e338697e4599185be602f1b79ff0
Steve <flip@foxhollow.cc>
2021-09-24 16:35:37 -0400

    add build scripts

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -1,24 +1,21 @@
-#!/bin/bash
-# requirements
-#   - dotnet
-#   - xmldoc2md (https://charlesdevandiere.github.io/xmldoc2md/)
-#   - dotnet tool install -g XMLDoc2Markdown
+#!/bin/sh
 
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 cd $SCRIPT_DIR/../
 
-dotnet build 
+cp ./Directory.Build.props ./docs/build_props.xml
 
-cd ./docs
-xmldoc2md ../src/bin/Debug/netstandard2.0/TerminalUI.dll ./api/ --examples-path examples/
+#dotnet build 
+
+# cd ./docs
+#xmldoc2md ../LibSMB2Sharp/bin/Debug/netstandard2.0/LibSMB2Sharp.dll ./api/ --examples-path examples/
 
 # here we will need to generate the api/_sidebar.md file from the input index.md file
-cd ./api
-cat ../_sidebar.md index.md \
-    | grep -v -e '^$' \
-    | grep -v -e '^# TerminalUI$' \
-    | sed 's/##/    -/' \
-    | sed 's/#/    -/' \
-    | sed 's/^\[/        - [/' > _sidebar.md
+#cd ./api
+#cat ../_sidebar.md index.md \
+#    | grep -v -e '^$' \
+#    | grep -v -e '^# TerminalUI$' \
+#    | sed 's/##/    -/' \
+#    | sed 's/#/    -/' \
+#    | sed 's/^\[/        - [/' > _sidebar.md
